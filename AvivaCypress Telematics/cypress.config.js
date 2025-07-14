76d927c5113510e93dceb8323b6020adff515b8f
Andy Robertson <a.robertson@dotsys.co.uk>
2025-07-14 15:05:43 +0100

    SYS UAT Logic Telematics

--- AvivaCypress Telematics/cypress.config.js
+++ AvivaCypress Telematics/cypress.config.js
@@ -4,7 +4,7 @@ require('cypress-mochawesome-reporter/plugin')
 async function setupNodeEvents(on, config) {
 
   const demoSpecPattern = [
-    'cypress/integration/aviva/Tests/001PolicyOwnership.cy.js',
+    'cypress/integration/aviva/Tests/000PolicyOwnership.cy.js',
     'cypress/integration/aviva/Tests/001TempPack.cy.js',
     // 'cypress/integration/aviva/Tests/002AgentRenewalPIFtoPIFNoEmail.cy.js',
     // 'cypress/integration/aviva/Tests/003ReportsCheck.cy.js',
