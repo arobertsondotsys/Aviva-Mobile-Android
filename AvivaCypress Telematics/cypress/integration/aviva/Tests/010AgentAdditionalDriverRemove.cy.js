c0af77ee60a4567e1b878dec70084368b7a2b702
Andy Robertson <a.robertson@dotsys.co.uk>
2025-07-24 09:53:35 +0100

    Telamtics fixes

--- AvivaCypress Telematics/cypress/integration/aviva/Tests/010AgentAdditionalDriverRemove.cy.js
+++ AvivaCypress Telematics/cypress/integration/aviva/Tests/010AgentAdditionalDriverRemove.cy.js
@@ -253,7 +253,7 @@ describe('Agent can add a driver and remove at ADJ', () => {
         PaymentTypesPage.paymentTypeAgentADJCardContinue()
 
         // Payment screen
-        PaymentPage.selectPaymentMethod()
+        PaymentPage.selectPaymentMethod1()
 
         // Thank you page
         ThankYouPage.thankyouHeading()
