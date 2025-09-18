38702cbf6cd14a48e63b86691be0743704e77da9
Andy Robertson <a.robertson@dotsys.co.uk>
2025-09-18 10:04:00 +0100

    Maintenance

--- AvivaCypressHome/cypress/integration/aviva/AvivaPOM/PaymentType.js
+++ AvivaCypressHome/cypress/integration/aviva/AvivaPOM/PaymentType.js
@@ -35,7 +35,7 @@ export class PaymentType{
 
     paymentTypeAgentDD(){
 
-        cy.getAndWait(this.LoginElementLocators.QuotePageLocators.paymenttype_agent).select(2)
+        cy.getAndWait(this.LoginElementLocators.QuotePageLocators.paymenttype_agent).select(3)
 
     }
 
