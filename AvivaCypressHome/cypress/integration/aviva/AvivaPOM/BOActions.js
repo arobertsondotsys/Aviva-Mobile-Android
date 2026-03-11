a0018ee8dbb2dfbc84bcba5b945c0a86f735f6f1
Andy Robertson <a.robertson@dotsys.co.uk>
2026-03-11 11:45:51 +0000

    cypress

--- AvivaCypressHome/cypress/integration/aviva/AvivaPOM/BOActions.js
+++ AvivaCypressHome/cypress/integration/aviva/AvivaPOM/BOActions.js
@@ -15,7 +15,7 @@ cookiesAccept(){
     //     }
     // })
 
-    cy.wait(1000)
+    cy.wait(3000)
     cy.get('body').then($body => {
         // Check if the cookie banner title is visible
         if ($body.find('#onetrust-policy-title:visible').length) {
